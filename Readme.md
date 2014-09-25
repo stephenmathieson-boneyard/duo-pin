f5c727263b5a33a7e10edfdd0feea9068680962e
Stephen Mathieson <smath23@gmail.com>
2014-09-25 15:15:45 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,48 @@
+
+# duo-pin
+
+  Duo subcommand for pinning dependencies.  Writes resolved versions to `./component.json`.
+
+## Usage
+
+```sh
+# build project
+$ duo index.js > build.js
+   
+   [ ... ]
+
+# pin resolved deps
+$ duo pin
+
+   reading : components/duo.json
+       pin : foo/bar@1.3.7
+       pin : bax/qux@0.0.1
+      dupe : foo/bar@2.0.0
+   writing : 2 dependencies to component.json
+
+```
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2014 Stephen Mathieson &lt;me@stephenmathieson.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ pin.js
@@ -0,0 +1,128 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+var debug = require('debug')('duo-pin');
+var Logger = require('stream-log');
+var fs = require('fs');
+var path = require('path');
+var fmt = require('util').format;
+
+/**
+ * Quiet flag.
+ */
+
+var quiet = !!~process.argv.indexOf('-q')
+         || !!~process.argv.indexOf('--quiet');
+
+/**
+ * Logger.
+ */
+
+var logger = new Logger(process.stderr);
+
+if (quiet) {
+  // noop
+  debug('disabling all logging');
+  logger.reading =
+  logger.pin =
+  logger.dupe =
+  logger.writing = function(){};
+} else {
+  logger
+  .type('reading', '36m')
+  .type('pin', '36m')
+  .type('writing', '36m')
+  .type('dupe', '33m');
+}
+
+/**
+ * Error.
+ */
+
+logger.type('error', '31m', function () {
+  logger.end();
+  process.exit(1);
+});
+
+/**
+ * Resolve duo.json.
+ */
+
+var root = process.cwd();
+var duo = path.join(root, 'components', 'duo.json');
+
+/**
+ * Read duo.json.
+ */
+
+logger.reading('components/duo.json');
+try {
+  var manifest = require(duo);
+} catch (err) {
+  debug(err);
+  if ('MODULE_NOT_FOUND' == err.code) {
+    logger.error(
+        'unable to locate components/duo.json: '
+      + 'you must run `duo` before running `duo pin`.'
+    );
+  }
+  throw err;
+}
+
+/**
+ * Build `dependencies: {}`.
+ */
+
+var json = { dependencies: {} };
+for (var component in manifest) {
+  // skip components that don't start with 'components'
+  if (0 != component.indexOf('components')) {
+    debug('skip local component: %s', component);
+    continue;
+  }
+  // parse/add dep
+  var parsed = dependency(component);
+  var str = fmt('%s@%s', parsed.component, parsed.version);
+  if (!json.dependencies[parsed.component]) {
+    json.dependencies[parsed.component] = parsed.version;
+    logger.pin(str);
+  } else {
+    logger.dupe(str);
+  }
+}
+
+/**
+ * Write.
+ */
+
+var c8 = path.join(root, 'component.json');
+debug('component.json: %s', c8);
+logger.writing(fmt(
+    '%d dependencies to component.json'
+  , Object.keys(json.dependencies).length
+));
+fs.writeFileSync(c8, JSON.stringify(json, null, 2));
+
+if (!quiet) logger.end();
+
+/**
+ * Parse `id` into a dependency.
+ *
+ * @api private
+ * @param {String} id
+ * @return {Object}
+ */
+
+function dependency(id) {
+  var slug = id.split('/')[1].split('@')
+  var name = slug[0];
+  var version = slug[1];
+  var p = name.split('-');
+  return {
+    component: fmt('%s/%s', p.shift(), p.join('-')),
+    version: version,
+  };
+}
